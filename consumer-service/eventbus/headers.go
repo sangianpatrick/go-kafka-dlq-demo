9918c094ed24b062ac20ac86dff24ba074904a07
Patrick Maurits Sangian <patricksangian@gmail.com>
2021-06-30 22:04:03 +0700

    chore(dlq-service): add slide presentation

--- consumer-service/eventbus/headers.go
+++
@@ -1,9 +0,0 @@
-package eventbus
-
-// MessageHeaders is type of message headers
-type MessageHeaders map[string]string
-
-// Add will add the key and value to headers.
-func (mh MessageHeaders) Add(key, value string) {
-	mh[key] = value
-}
